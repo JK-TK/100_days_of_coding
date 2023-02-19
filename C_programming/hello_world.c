7f41f70556b46b83933b557a33bd57ec16f84af7
JK <jamexkarix583@gmail.com>
2023-02-20 00:28:22 +0300

    simple coding

---
+++ C_programming/hello_world.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+#define NAME "James_kariuki"
+
+
+
+                           
+// 100_days_of_coding
+
+int main ()
+{
+	int a;
+
+	int c,Area;
+
+	printf["Input two datas %f %f",a,c];
+
+	//input data
+	//
+	
+	scanf(%f%f,&a,&c);
+
+	//simple.calculation
+	//
+	
+	Area = a*c;
+
+	printf("%d", Area);
+
+	//simole logic math
+	//
+	
+
+	if (a => c)
+
+		printf("%s","Success");
+
+	else 
+		printf("%s","Good_work");
+
+}
---
+++ C_programming/hi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define GREETINGS "Good_Evening"
+
+//Good evening
+//
+//One hundrend.days.of.coding
+//
+
+int main ()
+{
+
+	char a[30];
+
+	//Iput your name
+	//
+	printf("Input your name");
+	
+	scanf("%s",&a);
+
+	printf("%s%s",GREETINGS,a);
+
+}
