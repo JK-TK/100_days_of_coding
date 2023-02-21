da40776f62769fe88d452aa362cc0f4b7c6ff240
James kariuki_k <jamexkarix583@gmail.com>
2023-02-21 13:27:58 +0300

    21/02/2023

---
+++ C_programming/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rite a program that prints _putchar, followed by a new line.
+ * The programme should return 0
+ * Let us try this
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
